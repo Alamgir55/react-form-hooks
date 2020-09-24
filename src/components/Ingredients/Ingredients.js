2e41d78923470218c38bb460de467f0c61d75658
alamgir hossain <alamgir555hossain@gmail.com>
2020-09-24 19:47:15 -0400

    react-hooks-fetch

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ userIngredients, setUserIngredients ] = useState([]);
+
+  const addIngredientsHandler = Ingredient => {
+    fetch('https://react-hooks-9ae76.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(Ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [...prevIngredients, {id: responseData.name, ...Ingredient}])
+    })
+  } 
+  const removeIngredientsHandler = IngredientId => {
+    setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== IngredientId) );
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm  onAddIngredient={addIngredientsHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientsHandler} />
       </section>
     </div>
   );
