392746502d6f64351a9caa91e03e5c5af11d61d6
alamgir hossain <alamgir555hossain@gmail.com>
2020-09-25 19:50:26 -0400

    useState,useReducer,useRef

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,33 +1,80 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
+import ErrorModal from '../UI/ErrorModal';
+
+const ingredientReducer = (currentIngredient, action) => {
+    switch(action.type){
+      case 'SET':
+          return action.ingredients;
+      case 'ADD':
+          return [...currentIngredient, action.ingredient]
+      case 'DELETE':
+          return currentIngredient.filter(ing => ing.id !== action.id);
+      default: 
+        throw new Error('Should not get there');    
+    }
+}
 
 const Ingredients = () => {
-  const [ userIngredients, setUserIngredients ] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+
+  const filteredIngredientsHandler = useCallback( filterIngredients => {
+    dispatch({type: 'SET', ingredients: filterIngredients})
+  }, [])
+
+  useEffect(() => {
+    console.log('REDNDERING INGREDIENTS', userIngredients);
+  }, [userIngredients]);
 
   const addIngredientsHandler = Ingredient => {
+    setLoading(true);
     fetch('https://react-hooks-9ae76.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(Ingredient),
       headers: { 'Content-Type': 'application/json' }
     }).then(response => {
+      setLoading(false);
       return response.json();
     }).then(responseData => {
-      setUserIngredients(prevIngredients => [...prevIngredients, {id: responseData.name, ...Ingredient}])
+      dispatch({
+        type: 'ADD',
+        ingredient: {id: responseData.name, ...Ingredient}
+      })
     })
   } 
   const removeIngredientsHandler = IngredientId => {
-    setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== IngredientId) );
+    setLoading(true);
+    fetch(`https://react-hooks-9ae76.firebaseio.com/ingredients/${IngredientId}.json`, {
+      method: 'DELETE'
+    }).then(response => {
+      setLoading(false);
+      dispatch({
+        type: 'DELETE',
+        id: IngredientId
+      })
+    }).catch(error => {
+      setError('Something went wrong!');
+      setLoading(false);
+    })
+  }
+
+  const clearError = () => {
+    setError(null);
   }
 
   return (
     <div className="App">
-      <IngredientForm  onAddIngredient={addIngredientsHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm  onAddIngredient={addIngredientsHandler} loading={isLoading} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientsHandler} />
       </section>
     </div>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const { onLoadIngredients } = props;
+  const [ enterFilter, setEnterFilter ] = useState('');
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if(enterFilter === inputRef.current.value){
+        const query = enterFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enterFilter}"`;
+    
+        fetch('https://react-hooks-9ae76.firebaseio.com/ingredients.json' + query).then(response => response.json())
+              .then(responseData => {
+                const loadedIngredients = [];
+                for(const key in responseData){
+                  loadedIngredients.push({
+                    id: key,
+                    title: responseData[key].title,
+                    amount: responseData[key].amount
+                  })
+                }
+                 onLoadIngredients(loadedIngredients);
+              })
+      }
+    }, 500);
+    
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [enterFilter, onLoadIngredients, inputRef])
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+          ref={inputRef}
+          type="text" value={enterFilter} onChange={event => {
+            setEnterFilter(event.target.value);
+          }} />
         </div>
       </Card>
     </section>
